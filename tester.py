a1dbfc9bfd2afa43f261c6efb115ac698ff55a5b
fltls <royaljunseo1020@gmail.com>
2024-11-15 03:23:50 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -7,3 +7,13 @@ print('c =', c)
 r = 10
 area = utils.circle(r)
 print('area =', area)
+import math
+
+def pythagoras(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def circle(r):
+    area = math.pi * r**2
+    return area
+__all__ = ['pythagoras', 'circle']
